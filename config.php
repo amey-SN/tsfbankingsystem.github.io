212ac202dc6eba9195fedabfbc09b4821bdb9931
Amey Nagmode <ameynagmode@gmail.com>
2021-06-08 20:30:55 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost:3307"; //server name
-$username = "root"; //username of server
-$password=""; //password if required (generally not required for localhost database)
-$db_name="user_table"; //database name
+$username = "id16961631_root"; //username of server
+$password="$[*0~WR!QLUNqObb"; //password if required (generally not required for localhost database)
+$db_name="id16961631_bankusers"; //database name
 $conn = mysqli_connect($host, $username, $password, $db_name); //conecting to database
 if(!$conn){ //checking for connectivity
   echo "Connection Error";
